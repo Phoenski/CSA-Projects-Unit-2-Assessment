18815c65df1767f14d89c2a7aea5923ea0925c0e
Phoenski <69543226+Phoenski@users.noreply.github.com>
2021-10-11 23:22:48 -0400

    a

---
+++ CalculatorV3.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class CalculatorV3
+{
+    public static void main(String [] args)
+    {
+        // Variables
+        double varA;
+        double varB;
+        double varC;
+        double varD;
+        double varE;
+        double varF;
+        double varG;
+        double varH;
+        double varI;
+        double varJ;
+
+        // Scanner creation
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter variable A: ");
+        varA = scanner.nextDouble();
+        System.out.println("Enter variable B: ");
+        varB = scanner.nextDouble();
+        System.out.println("Enter variable C: ");
+        varC = scanner.nextDouble();
+        System.out.println("Enter variable D: ");
+        varD = scanner.nextDouble();
+        System.out.println("Enter variable E: ");
+        varE = scanner.nextDouble();
+        System.out.println("Enter variable F: ");
+        varF = scanner.nextDouble();
+        System.out.println("Enter variable G: ");
+        varG = scanner.nextDouble();
+        System.out.println("Enter variable H: ");
+        varH = scanner.nextDouble();
+        System.out.println("Enter variable I: ");
+        varI = scanner.nextDouble();
+        System.out.println("Enter variable J: ");
+        varJ = scanner.nextDouble();
+
+        if (varA == "varIgnore")
+        {
+            varA.();
+        }
+    }
+}
