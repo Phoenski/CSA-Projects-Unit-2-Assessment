b2f2afb83cc294994f00a6d1a214931a3b40e69b
Phoenski <69543226+Phoenski@users.noreply.github.com>
2021-10-24 22:43:38 -0400

    a

--- CalculatorV3.java
+++ CalculatorV3.java
@@ -1,6 +1,6 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class CalculatorV3
+class CalculatorV3
 {
     public static void main(String [] args)
     {
@@ -15,34 +15,152 @@ public class CalculatorV3
         double varH;
         double varI;
         double varJ;
+        int howManyVars;
 
         // Scanner creation
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter variable A: ");
-        varA = scanner.nextDouble();
-        System.out.println("Enter variable B: ");
-        varB = scanner.nextDouble();
-        System.out.println("Enter variable C: ");
-        varC = scanner.nextDouble();
-        System.out.println("Enter variable D: ");
-        varD = scanner.nextDouble();
-        System.out.println("Enter variable E: ");
-        varE = scanner.nextDouble();
-        System.out.println("Enter variable F: ");
-        varF = scanner.nextDouble();
-        System.out.println("Enter variable G: ");
-        varG = scanner.nextDouble();
-        System.out.println("Enter variable H: ");
-        varH = scanner.nextDouble();
-        System.out.println("Enter variable I: ");
-        varI = scanner.nextDouble();
-        System.out.println("Enter variable J: ");
-        varJ = scanner.nextDouble();
+        System.out.println("How many variables do you wish to uses? (2-10)");
+        howManyVars = scanner.nextInt();
 
-        if (varA == "varIgnore")
+        if (howManyVars == 2)
         {
-            varA.();
+            System.out.println("Enter variable A: ");
+            varA = scanner.nextDouble();
+            System.out.println("Enter variable B: ");
+            varB = scanner.nextDouble(); 
+        }
+        else if (howManyVars == 3)
+        {
+            System.out.println("Enter variable A: ");
+            varA = scanner.nextDouble();
+            System.out.println("Enter variable B: ");
+            varB = scanner.nextDouble();
+            System.out.println("Enter variable C: ");
+            varC = scanner.nextDouble();
+        }
+        else if (howManyVars == 4)
+        {
+            System.out.println("Enter variable A: ");
+            varA = scanner.nextDouble();
+            System.out.println("Enter variable B: ");
+            varB = scanner.nextDouble();
+            System.out.println("Enter variable C: ");
+            varC = scanner.nextDouble();
+            System.out.println("Enter variable D: ");
+            varD = scanner.nextDouble();
+        }
+        else if (howManyVars == 5)
+        {
+            System.out.println("Enter variable A: ");
+            varA = scanner.nextDouble();
+            System.out.println("Enter variable B: ");
+            varB = scanner.nextDouble();
+            System.out.println("Enter variable C: ");
+            varC = scanner.nextDouble();
+            System.out.println("Enter variable D: ");
+            varD = scanner.nextDouble();
+            System.out.println("Enter variable E: ");
+            varE = scanner.nextDouble();
+        }
+        else if (howManyVars == 6)
+        {
+            System.out.println("Enter variable A: ");
+            varA = scanner.nextDouble();
+            System.out.println("Enter variable B: ");
+            varB = scanner.nextDouble();
+            System.out.println("Enter variable C: ");
+            varC = scanner.nextDouble();
+            System.out.println("Enter variable D: ");
+            varD = scanner.nextDouble();
+            System.out.println("Enter variable E: ");
+            varE = scanner.nextDouble();
+            System.out.println("Enter variable F: ");
+            varF = scanner.nextDouble();
+        }
+        else if (howManyVars == 7)
+        {
+            System.out.println("Enter variable A: ");
+            varA = scanner.nextDouble();
+            System.out.println("Enter variable B: ");
+            varB = scanner.nextDouble();
+            System.out.println("Enter variable C: ");
+            varC = scanner.nextDouble();
+            System.out.println("Enter variable D: ");
+            varD = scanner.nextDouble();
+            System.out.println("Enter variable E: ");
+            varE = scanner.nextDouble();
+            System.out.println("Enter variable F: ");
+            varF = scanner.nextDouble();
+            System.out.println("Enter variable G: ");
+            varG = scanner.nextDouble();
+        }
+        else if (howManyVars == 8)
+        {
+            System.out.println("Enter variable A: ");
+            varA = scanner.nextDouble();
+            System.out.println("Enter variable B: ");
+            varB = scanner.nextDouble();
+            System.out.println("Enter variable C: ");
+            varC = scanner.nextDouble();
+            System.out.println("Enter variable D: ");
+            varD = scanner.nextDouble();
+            System.out.println("Enter variable E: ");
+            varE = scanner.nextDouble();
+            System.out.println("Enter variable F: ");
+            varF = scanner.nextDouble();
+            System.out.println("Enter variable G: ");
+            varG = scanner.nextDouble();
+            System.out.println("Enter variable H: ");
+            varH = scanner.nextDouble();
+        }
+        else if (howManyVars == 9)
+        {
+            System.out.println("Enter variable A: ");
+            varA = scanner.nextDouble();
+            System.out.println("Enter variable B: ");
+            varB = scanner.nextDouble();
+            System.out.println("Enter variable C: ");
+            varC = scanner.nextDouble();
+            System.out.println("Enter variable D: ");
+            varD = scanner.nextDouble();
+            System.out.println("Enter variable E: ");
+            varE = scanner.nextDouble();
+            System.out.println("Enter variable F: ");
+            varF = scanner.nextDouble();
+            System.out.println("Enter variable G: ");
+            varG = scanner.nextDouble();
+            System.out.println("Enter variable H: ");
+            varH = scanner.nextDouble();
+            System.out.println("Enter variable I: ");
+            varI = scanner.nextDouble();
+        }
+        else if (howManyVars == 10)
+        {
+            System.out.println("Enter variable A: ");
+            varA = scanner.nextDouble();
+            System.out.println("Enter variable B: ");
+            varB = scanner.nextDouble();
+            System.out.println("Enter variable C: ");
+            varC = scanner.nextDouble();
+            System.out.println("Enter variable D: ");
+            varD = scanner.nextDouble();
+            System.out.println("Enter variable E: ");
+            varE = scanner.nextDouble();
+            System.out.println("Enter variable F: ");
+            varF = scanner.nextDouble();
+            System.out.println("Enter variable G: ");
+            varG = scanner.nextDouble();
+            System.out.println("Enter variable H: ");
+            varH = scanner.nextDouble();
+            System.out.println("Enter variable I: ");
+            varI = scanner.nextDouble();
+            System.out.println("Enter variable J: ");
+            varJ = scanner.nextDouble();
+        }
+        else 
+        {
+            System.out.println("Invalid entry");
         }
     }
 }
